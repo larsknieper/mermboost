5095c8c6b1f5b803410fc97638cffcc452034964
Lars Knieper <88489668+larsknieper@users.noreply.github.com>
2024-10-22 09:53:51 +0200

    Delete R/zzz.R

--- R/zzz.R
+++
@@ -1,6 +0,0 @@
-.onLoad <- function(libname, pkgname) {
-  suppressPackageStartupMessages({
-    library(fastDummies, quietly = TRUE)
-    # Repeat for other libraries, even if they are in NAMESPACE
-  })
-}
